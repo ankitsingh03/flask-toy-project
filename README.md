495575ff6f9d5ce267251d059a3ca6a9816f54df
ankit <singh.ankit240@gmail.com>
2020-12-14 12:03:21 +0530

    dummy data

--- README.md
+++ README.md
@@ -11,14 +11,22 @@ You will implement the requirements which is written in the **USER-STORIES.md**.
 5. List of all the dependencies in **requirements.txt**
 
 ## How To Run
-1. Create virtual env for project.
-2. Install all the dependencies. Run `pip install -r requirements.txt`
-3. Run this to open postgres terminal `sudo -u postgres psql`
-4. Run **create.sql** file in postgres. Use `\i create.sql;`.
-5. Run the `flask db upgrade` in project terminal to create tables.
+1. Clone the project `git clone https://gitlab.com/mountblue/cohort-14-python/ankit_singh/flask-toy-project`
+2. Open terminal in project directory.
+3. Create virtual environment for project.
+4. Install all the dependencies. Run `pip install -r requirements.txt`
+5. Setup role and database in postgres.
+    * Run `sudo -u postgres psql`
+    * Create role and database `\i create.sql;`
+    * To close postgres `\q`
 6. Run the `export FLASK_APP=app.py` in terminal.
-7. Run the `flask run` in terminal.
-8. For clear up, role and database. Run this `\i drop.sql;` in postgres.
+7. Run the `flask db upgrade` in project terminal to create tables.
+8. Run both the command for dummy data `flask dummyuser` and `flask dummypost`.
+9. Run the `flask run` in terminal.
+10. For clean up role and database.
+    * Run `sudo -u postgres psql`
+    * Drop role and database `\i drop.sql;`
+    * To close postgres `\q`
 
 ## About Project
 Hello Seller and Buyer, we make this website to provide an ease for you in property dealing. We are here to help you to build a connection between seller and buyer in a very efficient way. Our vision is to remove the brokerage amount which is taking from the seller and buyer by broker. In this website you can easly connect with seller and get the info about any property.
--- app.py
+++ app.py
@@ -1,4 +1,55 @@
 from flaskproperty import app
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+@app.cli.command()
+def dummyuser():
+    from flaskproperty import db
+    from flaskproperty.models import User
+    seller1 = User(username='seller1',
+                   email="seller1@demo.com", password="123")
+    seller2 = User(username='seller2',
+                   email="seller2@demo.com", password="123")
+    db.session.add(seller1)
+    db.session.add(seller2)
+    db.session.commit()
+
+
+@app.cli.command()
+def dummypost():
+    from flaskproperty import db
+    from flaskproperty.models import User, Post
+    post1 = Post(location='Bangalore',
+                 detail="Enjoy a blissful living experience in JP North.\
+                 This residential project encompasses 1 BHK flats in Mira\
+                 Road East, Mira Road And Beyond and brings you the best \
+                 of both the worlds – excellent aesthetics and\
+                 exemplary lifestyle.Its unique highlights include\
+                 facilities like 24/7 Water Supply, Sewage Treatment Plant, \
+                 Lawn Tennis Court, And you can now buy your exclusive 1 BHK\
+                 flat in this project at a price of Rs. 44.56 Lac - Rs. 44.56\
+                 Lac.It has a carpet area ranging from 369.0 sq. ft. - 369.0\
+                 sq. ft. contact me: 9001210121",
+                 user_id=User.query
+                             .filter_by(email='seller1@demo.com')
+                             .first().id)
+    post2 = Post(location='Mumbai',
+                 detail="Enjoy a blissful living experience in JP North.\
+                 This residential project encompasses 1 BHK flats in Mira\
+                 Road East, Mira Road And Beyond and brings you the best \
+                 of both the worlds – excellent aesthetics and\
+                 exemplary lifestyle.Its unique highlights include\
+                 facilities like 24/7 Water Supply, Sewage Treatment Plant, \
+                 Lawn Tennis Court, And you can now buy your exclusive 1 BHK\
+                 flat in this project at a price of Rs. 44.56 Lac - Rs. 44.56\
+                 Lac.It has a carpet area ranging from 369.0 sq. ft. - 369.0\
+                 sq. ft. contact me: 9001210121",
+                 user_id=User.query
+                             .filter_by(email='seller2@demo.com')
+                             .first().id)
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
